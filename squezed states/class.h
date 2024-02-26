b93490f6bba4445558c73b8fb962c648791001e2
Veget <Veget@DESKTOP-PI0DL1E>
2024-02-26 20:57:01 +0100

    Dodaj pliki projektów.

---
+++ squezed states/class.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "armadillo"
+#include <iostream>
+using namespace std;
+
+class sqeeze {
+    // Access specifier 
+public:
+    // Data  Members 
+    string geekname;
+    // Member Functions() 
+    void printname() { cout << "Geekname is:" << geekname; }
+};
